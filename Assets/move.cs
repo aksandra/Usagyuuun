74ca0fd12a9acd1b42c8ef7a07b9006e2b331c9b
aksandra <aambrozy13@gmail.com>
2018-11-20 21:57:15 +0100

    2

---
+++ Assets/move.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour {
+
+	public float speed;
+	public float jump;
+	
+	public Transform groundCheck;
+	public float groundCheckRadius;
+	public LayerMask whatIsGround;
+	private bool grouned;
+	private bool doubleJump;
+	
+	void Start () {
+	}
+	
+	void FixedUpdate(){
+			grouned = Physics2D.OverlapCircle (groundCheck.position,groundCheckRadius,whatIsGround);
+			
+	}
+	
+	void Update () {
+		
+		if(grouned)
+		{
+		doubleJump = false;
+		}
+		
+		if(Input.GetKeyDown(KeyCode.W)&& grouned)
+		{
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (0,jump);
+		}
+		if(Input.GetKeyDown(KeyCode.W)&& !grouned && !doubleJump)
+		{
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (0,jump);
+		}
+		if(Input.GetKey(KeyCode.D))
+		{
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (speed,GetComponent<Rigidbody2D> ().velocity.y);
+		}
+		if(Input.GetKey(KeyCode.A))
+		{
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (-speed,GetComponent<Rigidbody2D> ().velocity.y);
+		}
+	}
+}
